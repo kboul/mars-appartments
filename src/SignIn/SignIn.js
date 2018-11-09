62bace765850ad6e5dde1aa000613779806eaa64
kboul <kos.voulgaridis@gmail.com>
2018-11-09 17:10:02 +0200

    Refactor SignIn comp & retrieve token by making a login request

--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -1,40 +1,60 @@
 import React from 'react';
 import Joi from 'joi-browser';
 import Form from '../common/Form';
+import authService from '../services/authService';
 
 import './SignIn.sass';
 
 class SignIn extends Form {
     state = {
         data: {
-            colonistId: '',
+            email: '',
             password: ''
         },
         errors: {}
     }
 
     schema = {
-        colonistId: Joi
+        email: Joi
             .string()
             .email({ minDomainAtoms: 2 })
             .required()
             .label("Colonist ID"),
         password: Joi
-            .number()
+            .string()
             .required()
             .label("Password")
+    }
+
+    doSubmit = async () => {
+        try {
+            const { data } = this.state;
+            await authService.login(data.email, data.password);
+
+            // const { state } = this.props.location;
+            // window.location = state ? state.from.pathname : '/';
+        } catch (ex) {
+            if (ex.response && ex.response.status === 400) {
+                const errors = {...this.state.errors};
+                errors.username = ex.response.data;
+                this.setState({ errors });
+            }
+        }
     }
 
     render() { 
         return ( 
             <div className="container authenticate-container">
-                <form className="form-authenticate" noValidate>
+                <form 
+                    className="form-authenticate" 
+                    noValidate
+                    onSubmit={this.handleSubmit}>
                     <div className="card rounded-0">
                         <div className="card-body">
                             <div className="mb-4">
                                 <b>Blueground on Mars</b>
                             </div>
-                            {this.renderInput("colonistId", "Colonist ID")}
+                            {this.renderInput("email", "Colonist ID")}
                             {this.renderInput("password", "Password")}
                             <div className="float-right">
                                 {this.renderButton("Sign In")}
