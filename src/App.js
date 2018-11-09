b4b3283e76738823870f7848c00bd6e5de18b702
kboul <kos.voulgaridis@gmail.com>
2018-11-09 17:27:24 +0200

    Create Browse comp & register it on Routes on App comp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import SignIn from './components/SignIn/SignIn';
+import Browse from './components/Browse/Browse';
 
 import './App.sass';
 
@@ -9,9 +10,9 @@ class App extends Component {
 	render() {
 		return (
 			<Switch>
-				{/* <Route path="/browse" component={Browse} /> */}
-				<Redirect from="/" exact to="/signin" />
+				<Route path="/browse" component={Browse} />
 				<Route path="/signin" component={SignIn} />
+				<Redirect from="/" exact to="/signin" />
 			</Switch>
 		);
 	}
