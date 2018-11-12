91682cb1a986de850a5305c40226aa11a6475529
kboul <kos.voulgaridis@gmail.com>
2018-11-12 18:02:04 +0200

    Make modal to occupy full 100% height & 50% width on the right

--- src/components/ModalComp/ModalComp.js
+++ src/components/ModalComp/ModalComp.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Modal, ModalBody } from 'reactstrap';
+import './ModalComp.sass';
+
+const modalStyle = { 
+    width: '50%',
+    height: '100%', 
+    marginRight: '0px',
+    marginTop: '0px',
+    marginBottom: '0px'
+};
 
 const ModalComp = ({modal, toggle, children}) => {
 	return ( 
-		<Modal 
+        <Modal 
+            style={modalStyle}
 			isOpen={modal} 
-			toggle={toggle}
+            toggle={toggle}
 			size="lg">
-			<ModalBody>{children}</ModalBody>
+			<ModalBody >{children}</ModalBody>
         </Modal>
 	);
 }
