132cc751a20e17916fac27b26c17685c7c019e16
kboul <kos.voulgaridis@gmail.com>
2018-11-12 18:21:08 +0200

    Create unit service

---
+++ src/services/unitService.js
@@ -0,0 +1,8 @@
+import http from '../services/httpService';
+import { getHeaders } from "./authService";
+
+const apiEndpoint = '/units';
+
+export function getUnit(id) {
+    return http.get(`${apiEndpoint}/${id}`, getHeaders());
+}
