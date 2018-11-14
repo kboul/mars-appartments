a623b4b0c27fb560f9bdee5b1de3cfe356033b05
kboul <kos.voulgaridis@gmail.com>
2018-11-14 12:03:39 +0200

    Highlight email input field once users types a valid email value on SignIn comp

--- src/common/Input.js
+++ src/common/Input.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+const success = { boxShadow: '0 0 5px 1px green' };
+
 const Input = ({ name, label, value, error, onChange }) => {
     return ( 
         <div className="form-group">
             <input
+                style={
+                    !error && 
+                    name === "email" && 
+                    value !== '' ? success : null
+                }
                 value={value}
                 onChange={onChange}
                 type="text" 
