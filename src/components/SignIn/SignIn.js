ac498cdebb672b9d80f168600a580b624ee1585c
kboul <kos.voulgaridis@gmail.com>
2018-11-14 11:29:05 +0200

    Fix a comment on SignIn comp

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -36,7 +36,7 @@ class SignIn extends Form {
         try {
             const { data } = this.state;
             await authService.login(data.email, data.password);
-            // land to unit view if successful
+            // land to units view if successful
             this.props.history.push('/units');
         } catch (ex) {
             if (ex.response && ex.response.status === 400) {
