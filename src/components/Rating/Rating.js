44663c5d2735c9a7f619319f459a69c1b92ee188
kboul <kos.voulgaridis@gmail.com>
2019-05-07 18:25:37 +0300

    Remove semicolons from Rating & remove isRequired from stars prop

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactStars from 'react-stars';
-import PropTypes from 'prop-types'; 
+import React from 'react'
+import ReactStars from 'react-stars'
+import PropTypes from 'prop-types'
 
-const Rating = ({stars}) => {
-    return ( 
+const Rating = ({ stars }) => {
+    return (
         <ReactStars
             count={5}
             value={stars}
             size={24}
             color1={'#DCDCDC'}
             color2={'#000000'} />
-    );
-};
+    )
+}
 
 Rating.propTypes = {
-    stars: PropTypes.number.isRequired
-};
+    stars: PropTypes.number
+}
 
-export default Rating;
+export default Rating
