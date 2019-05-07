e6447ade126aefe4f8f8c226adfbd7de4dbe7325
kboul <kos.voulgaridis@gmail.com>
2019-05-07 18:24:55 +0300

    Place utils folder under src & correct related paths in ather files

--- src/components/ModalContent/ModalContent.js
+++ src/components/ModalContent/ModalContent.js
@@ -1,90 +1,88 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import Rating from '../Rating/Rating'
+import { getUnit } from '../../services/unitService'
+import { euro2Bitcoin } from '../../utils/euro2Bitcoin'
+import './ModalContent.sass'
 
-import { getUnit } from '../../services/unitService';
-import { euro2Bitcoin } from '../utils/euro2Bitcoin';
-import Rating from '../Rating/Rating';
-
-import './ModalContent.sass';
-
-const buttonsStyle = { cursor: 'none' };
-const imgUrlPrefix = process.env.REACT_APP_IMG_URL_PREFIX;
+const buttonsStyle = { cursor: 'none' }
+const imgUrlPrefix = process.env.REACT_APP_IMG_URL_PREFIX
 
 class ModalContent extends Component {
-    state = { 
+    state = {
         unit: {},
         amenities: [],
         availability: []
     }
 
     async componentDidMount() {
-        const userId = this.props.match.params.id;
-        const { data: unit } = await getUnit(userId);
-        const amenities = unit.amenities;
-        const availability = unit.availability;
-        this.setState({ unit, amenities, availability });
-        // console.log(this.state.unit);
+        const userId = this.props.match.params.id
+        const { data: unit } = await getUnit(userId)
+        const amenities = unit.amenities
+        const availability = unit.availability
+        this.setState({ unit, amenities, availability })
+        // console.log(this.state.unit)
     }
 
     createMarkup(html) {
-        return {__html: `<i>Description</i>: ${html}`};
+        return { __html: `<i>Description</i>: ${html}` }
     }
 
     findBiggestImg(picturesArray) {
-        return picturesArray.find(element => element.includes('w800'));
+        return picturesArray.find(element => element.includes('w800'))
     }
 
-    render() { 
-        const { unit, amenities, availability } = this.state;
-    
-        return ( 
+    render() {
+        const { unit, amenities, availability } = this.state
+
+        return (
             <React.Fragment>
-                <div 
+                <div
                     className="row"
                     id="modalImage">
-                    {unit && unit.pictures && 
-                    <img 
-                        src={`${imgUrlPrefix}/${this.findBiggestImg(unit.pictures)}`} 
-                        alt={`${imgUrlPrefix}/${this.findBiggestImg(unit.pictures)}`} />
+                    {unit && unit.pictures &&
+                        <img
+                            src={`${imgUrlPrefix}/${this.findBiggestImg(unit.pictures)}`}
+                            alt={`${imgUrlPrefix}/${this.findBiggestImg(unit.pictures)}`} />
                     }
                 </div>
-                
-                <div 
-                    id="nameRegion" 
+
+                <div
+                    id="nameRegion"
                     className="d-flex justify-content-between">
                     <div>
                         <b> {unit.name} - {unit.region}</b>
-                    </div> 
+                    </div>
                     <div>
                         <button
-                            type="button" 
+                            type="button"
                             className="btn btn-secondary special-btn"
-                            style={{width: '100px', ...buttonsStyle}}>
+                            style={{ width: '100px', ...buttonsStyle }}>
                             <b>{euro2Bitcoin(unit.price)}</b>
                         </button>
                     </div>
                 </div>
-                
+
                 <Rating stars={unit.rating} />
 
-                
-                <div 
-                    id="description" 
+
+                <div
+                    id="description"
                     dangerouslySetInnerHTML={this.createMarkup(unit.description)} />
-                
+
                 <div id="amenities">
-                    <b>Amenities: </b> 
+                    <b>Amenities: </b>
                     {amenities.join(', ')}
                 </div>
 
                 <div className="row">
                     {availability.map(year => {
                         return (
-                            <div 
-                                className="col-12 col-sm-3 col-md-3" 
+                            <div
+                                className="col-12 col-sm-3 col-md-3"
                                 key={year}>
                                 <button
-                                    style={{marginBottom: '8px', ...buttonsStyle}} 
+                                    style={{ marginBottom: '8px', ...buttonsStyle }}
                                     className="btn btn-light btn-block special-btn">
                                     {year}
                                 </button>
@@ -93,14 +91,14 @@ class ModalContent extends Component {
                     })}
                 </div>
 
-                <button 
-                    type="button" 
+                <button
+                    type="button"
                     className="btn btn-secondary btn-dark book-btn float-right">
                     Book
                 </button>
             </React.Fragment>
-        );
+        )
     }
 }
- 
-export default withRouter(ModalContent);
+
+export default withRouter(ModalContent)
--- src/components/Units/Units.js
+++ src/components/Units/Units.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
-import { getUnits } from '../../services/unitsService';
-import { euro2Bitcoin } from '../utils/euro2Bitcoin';
-import { getUserImg, getUserName } from '../../services/authService';
+import { getUnits } from '../../services/unitsService'
+import { euro2Bitcoin } from '../../utils/euro2Bitcoin'
+import { getUserImg, getUserName } from '../../services/authService'
 
-import MarsCircleHeader from '../MarsCircleHeader/MarsCircleHeader';
-import ModalComp from '../ModalComp/ModalComp';
-import ModalContent from '../ModalContent/ModalContent';
-import Rating from '../Rating/Rating';
-import Search from '../Search/Search';
-import UserAvatar from '../UserAvatar/UserAvatar';
+import MarsCircleHeader from '../MarsCircleHeader/MarsCircleHeader'
+import ModalComp from '../ModalComp/ModalComp'
+import ModalContent from '../ModalContent/ModalContent'
+import Rating from '../Rating/Rating'
+import Search from '../Search/Search'
+import UserAvatar from '../UserAvatar/UserAvatar'
 
-const cardStyle = { border: '0px' };
-const cardBodyStyle = { padding: '0' };
-const clickableCard = { cursor: 'pointer' };
-const imgUrlPrefix = process.env.REACT_APP_IMG_URL_PREFIX;
+const cardStyle = { border: '0px' }
+const cardBodyStyle = { padding: '0' }
+const clickableCard = { cursor: 'pointer' }
+const imgUrlPrefix = process.env.REACT_APP_IMG_URL_PREFIX
 
 class Units extends Component {
     state = {
@@ -27,92 +27,92 @@ class Units extends Component {
     }
 
     async componentDidMount() {
-        const { data: units } = await getUnits();
-        const userImg = getUserImg();
-        const userName = getUserName();
-        this.setState({ units: units.data, userImg, userName });
-        console.log(this.state.units);
+        const { data: units } = await getUnits()
+        const userImg = getUserImg()
+        const userName = getUserName()
+        this.setState({ units: units.data, userImg, userName })
+        console.log(this.state.units)
     }
 
     toggle = () => {
         this.setState({
             modal: !this.state.modal
-        });
+        })
     }
 
     addIdToUrl = (id) => {
-        this.props.history.push(`/units/${id}`);
+        this.props.history.push(`/units/${id}`)
     }
 
     handleClick = (id) => {
-        this.toggle();
-        this.addIdToUrl(id);
+        this.toggle()
+        this.addIdToUrl(id)
     }
 
     handleSearch = ({ currentTarget: input }) => {
-        const searchQuery = input.value;
-        this.setState({ searchQuery });
+        const searchQuery = input.value
+        this.setState({ searchQuery })
     }
 
-    render() { 
-        const { userImg, userName, searchQuery, modal, units } = this.state;
-        const filteredUnits = units.filter(unit => 
-            unit.name.toLowerCase().includes(searchQuery) || 
+    render() {
+        const { userImg, userName, searchQuery, modal, units } = this.state
+        const filteredUnits = units.filter(unit =>
+            unit.name.toLowerCase().includes(searchQuery) ||
             unit.region.toLowerCase().includes(searchQuery)
-        );
+        )
 
-        return ( 
+        return (
             <div className="container-fluid">
                 <div className="d-flex justify-content-between mt-2 mb-4">
                     <div>
-                        <MarsCircleHeader 
-                            background="white" 
+                        <MarsCircleHeader
+                            background="white"
                             color="black" />
                     </div>
-                    <UserAvatar 
+                    <UserAvatar
                         image={userImg}
                         name={userName} />
                 </div>
 
-                <Search 
-                    value={searchQuery} 
+                <Search
+                    value={searchQuery}
                     onSearch={this.handleSearch}
                     itemsDisplayed={filteredUnits.length} />
 
                 <div className="row mt-2">
                     <ModalComp
-                        modal={modal} 
+                        modal={modal}
                         toggle={this.toggle}>
-                       <ModalContent />   
-                    </ModalComp>  
-                    {filteredUnits.map(({id, pictures, name, region, description, cancellation, price, rating}) => {
+                        <ModalContent />
+                    </ModalComp>
+                    {filteredUnits.map(({ id, pictures, name, region, description, cancellation, price, rating }) => {
                         return (
-                            <div 
-                                className="col-md-4" 
+                            <div
+                                className="col-md-4"
                                 key={id}
                                 onClick={() => this.handleClick(id)}
-                                style={clickableCard}>   
-                                <div 
-                                    className="card mb-4 box-shadow" 
+                                style={clickableCard}>
+                                <div
+                                    className="card mb-4 box-shadow"
                                     style={cardStyle}>
-                                    <img 
-                                        className="card-img-top" 
-                                        src={`${imgUrlPrefix}/${pictures[1]}`} 
+                                    <img
+                                        className="card-img-top"
+                                        src={`${imgUrlPrefix}/${pictures[1]}`}
                                         alt={`${imgUrlPrefix}/${pictures[1]}`} />
-                                    <div 
-                                        className="card-body" 
+                                    <div
+                                        className="card-body"
                                         style={cardBodyStyle}>
-                                        <b>{name}</b> 
+                                        <b>{name}</b>
                                         <div>{region}</div>
                                         <div className="card-text">
-                                            {`${description.substring(0,60)}...`}
+                                            {`${description.substring(0, 60)}...`}
                                         </div>
-                                        <div>{cancellation}</div> 
+                                        <div>{cancellation}</div>
                                         <Rating stars={rating} />
                                         <div className="card-text">
                                             {/* <b>{(price / 5600).toFixed(2)} BTC</b> */}
                                             <b> {euro2Bitcoin(price)}</b>
-                                        </div>      
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -120,8 +120,8 @@ class Units extends Component {
                     })}
                 </div>
             </div>
-        );
+        )
     }
 }
- 
-export default withRouter(Units);
+
+export default withRouter(Units)
