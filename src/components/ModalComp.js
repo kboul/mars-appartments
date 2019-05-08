5ccefe63345db4002278df8b30d926673c6cc7eb
kboul <kos.voulgaridis@gmail.com>
2019-05-08 11:42:28 +0300

    Remove style as a variable on ModalComp & place it inside ModalComp.sass

--- src/components/ModalComp.js
+++ src/components/ModalComp.js
@@ -3,22 +3,15 @@ import { Modal, ModalBody } from 'reactstrap'
 import PropTypes from 'prop-types'
 import '../sass/ModalComp.sass'
 
-const modalStyle = {
-    width: '50%',
-    height: '100%',
-    marginRight: '0px',
-    marginTop: '0px',
-    marginBottom: '0px'
-}
-
 const ModalComp = ({ modal, toggle, children }) => {
     return (
         <Modal
-            style={modalStyle}
             isOpen={modal}
             toggle={toggle}
             size="lg">
-            <ModalBody >{children}</ModalBody>
+            <ModalBody>
+                {children}
+            </ModalBody>
         </Modal>
     )
 }
