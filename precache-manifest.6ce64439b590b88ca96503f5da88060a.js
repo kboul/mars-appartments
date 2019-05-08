cc421a2cd1fdc08fea6a34b8b6f31591846f2673
kboul <kos.voulgaridis@gmail.com>
2019-05-08 13:05:05 +0300

    Updates

---
+++ precache-manifest.6ce64439b590b88ca96503f5da88060a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "86aeb777d2d26331f6cf",
+    "url": "/mars-app/static/css/main.dd75ee04.chunk.css"
+  },
+  {
+    "revision": "86aeb777d2d26331f6cf",
+    "url": "/mars-app/static/js/main.4b9da029.chunk.js"
+  },
+  {
+    "revision": "7c4d1f1dc6d3f5a2ac83",
+    "url": "/mars-app/static/js/runtime~main.b512b9ac.js"
+  },
+  {
+    "revision": "6386d8cefd4097f0bc49",
+    "url": "/mars-app/static/css/2.01feb47e.chunk.css"
+  },
+  {
+    "revision": "6386d8cefd4097f0bc49",
+    "url": "/mars-app/static/js/2.c1a1be26.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mars-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mars-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mars-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mars-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mars-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d2a9cd9352e8e9c3c62903779258048f",
+    "url": "/mars-app/index.html"
+  }
+];
