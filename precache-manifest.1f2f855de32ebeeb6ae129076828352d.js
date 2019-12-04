34a6ee9fcc6a555a1b1d96b43892ae623d493d5f
kboul <kos.voulgaridis@gmail.com>
2019-12-04 11:42:13 +0200

    Updates

---
+++ precache-manifest.1f2f855de32ebeeb6ae129076828352d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfce5fa2783b403e0ce02385778e4859",
+    "url": "/mars-app/index.html"
+  },
+  {
+    "revision": "36ff426bbd168c603d2b",
+    "url": "/mars-app/static/css/2.2323ec8e.chunk.css"
+  },
+  {
+    "revision": "4f560da2bbaf719e4dd3",
+    "url": "/mars-app/static/css/main.1c1f5936.chunk.css"
+  },
+  {
+    "revision": "36ff426bbd168c603d2b",
+    "url": "/mars-app/static/js/2.ec491f80.chunk.js"
+  },
+  {
+    "revision": "4f560da2bbaf719e4dd3",
+    "url": "/mars-app/static/js/main.020b3fd9.chunk.js"
+  },
+  {
+    "revision": "75eb78351ff0f91e5604",
+    "url": "/mars-app/static/js/runtime-main.0f52e5c7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mars-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mars-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mars-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mars-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mars-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mars-app/precache-manifest.95b518e9911bfab13151202ef17b1033.js"
+  "/mars-app/precache-manifest.1f2f855de32ebeeb6ae129076828352d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mars-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mars-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
